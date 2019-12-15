aa0e8ed1eeb493c433fc051295e2d30ca0684d32
sachin kesarkar <devops.sachin@gmail.com>
2019-12-15 22:12:30 +0530

    added inital files

---
+++ Mumshad/MyProjects/venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!"D:\1_SAP laptop Backup 23Sep2019 - MyWorkSpace\Sachin_Folder\1_Udemy_CKA_certification\MM\MyProjects\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip')()
+    )
